534b8856d382a9844d1f5ccfcd45cb9368fedd62
Manoj Kumar <manojkr.itbhu@gmail.com>
2016-04-02 15:51:49 +0530

    Chapter3

---
+++ homework/chapter3/dependencies.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var fx = require('./fx');
+var Stripe = require('stripe');
+
+module.exports = function(wagner) {
+    wagner.factory('Config', function() {
+	return JSON.parse(fs.readFileSync('./config.json').toString());
+    });
+    
+	
+	// TODO: Make Stripe depend on the Config service and use its `stripeKey`
+	wagner.invoke(function(Config) {
+	    // property to get the Stripe API key.
+	    wagner.factory('Stripe', function() {
+		return Stripe(Config.stripeKey);
+	    });
+	});
+
+    wagner.factory('fx', fx);
+
+};
